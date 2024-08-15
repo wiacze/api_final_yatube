b2bbe7c2c6f77b43e238a12fd1085803ade2ad55
wiacze <151467867+wiacze@users.noreply.github.com>
2024-08-15 20:30:38 +0300

    First release

--- yatube_api/api/serializers.py
+++ yatube_api/api/serializers.py
@@ -1,23 +1,54 @@
 from rest_framework import serializers
 from rest_framework.relations import SlugRelatedField
+from rest_framework.validators import UniqueTogetherValidator
+
+from posts.models import Comment, Post, Group, Follow, User
 
 
-from posts.models import Comment, Post
-
-
-class PostSerializer(serializers.ModelSerializer):
+class AuthorFieldMixin(serializers.ModelSerializer):
     author = SlugRelatedField(slug_field='username', read_only=True)
+
+
+class PostSerializer(AuthorFieldMixin):
 
     class Meta:
         fields = '__all__'
         model = Post
 
 
-class CommentSerializer(serializers.ModelSerializer):
-    author = serializers.SlugRelatedField(
-        read_only=True, slug_field='username'
-    )
+class CommentSerializer(AuthorFieldMixin):
+    post = serializers.SlugRelatedField(read_only=True, slug_field='id')
 
     class Meta:
         fields = '__all__'
         model = Comment
+
+
+class GroupSerializer(serializers.ModelSerializer):
+    class Meta:
+        fields = '__all__'
+        model = Group
+
+
+class FollowSerializer(serializers.ModelSerializer):
+    user = SlugRelatedField(
+        read_only=True, slug_field='username',
+        default=serializers.CurrentUserDefault())
+    following = SlugRelatedField(
+        queryset=User.objects.all(), slug_field='username')
+
+    class Meta:
+        fields = '__all__'
+        model = Follow
+        validators = [
+            UniqueTogetherValidator(
+                queryset=Follow.objects.all(),
+                fields=('user', 'following')
+            )
+        ]
+
+    def validate(self, data):
+        if data['following'] == self.context['request'].user:
+            raise serializers.ValidationError(
+                'Нельзя подписать на самого себя.')
+        return data
--- yatube_api/api/urls.py
+++ yatube_api/api/urls.py
@@ -1,4 +1,18 @@
-from django.urls import path
+from rest_framework.routers import DefaultRouter
+from django.urls import include, path
+
+from .views import GroupViewSet, PostViewSet, CommentViewSet, FollowViewSet
+
+
+router = DefaultRouter()
+
+router.register('groups', GroupViewSet, 'groups')
+router.register('posts', PostViewSet, 'posts')
+router.register(r'posts/(?P<post_id>\d+)/comments',
+                CommentViewSet, 'comments')
+router.register('follow', FollowViewSet, 'follows')
+
 
 urlpatterns = [
+    path('', include(router.urls)),
 ]
