90161a624846f1a9bdeeacde8a180e0777247289
wiacze <151467867+wiacze@users.noreply.github.com>
2024-08-16 14:57:13 +0300

    Simplified

--- yatube_api/api/permissions.py
+++ yatube_api/api/permissions.py
@@ -5,6 +5,7 @@ class IsAuthorOrReadOnly(permissions.IsAuthenticatedOrReadOnly):
     message = 'Изменение чужого контента запрещено!'
 
     def has_object_permission(self, request, view, obj):
-        if request.method in permissions.SAFE_METHODS:
-            return True
-        return obj.author == request.user
+        return (
+            request.method in permissions.SAFE_METHODS
+            or obj.author == request.user
+        )
